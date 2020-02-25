7f8ff4789de9680a3e0bfe1b7e5fea6ae17bcc3e
David Nassif <David.Nassif@NexTierOFS.com>
2020-02-25 13:55:08 -0600

    this is the new structure and started app.js

--- webpage/static/js/app.js
+++ webpage/static/js/app.js
@@ -1,31 +1,6 @@
-// from data.js
-var tableData = data;
+var url='http://127.0.0.1:5000/api/v1.0/all_winners_data'
 
-var tbody = d3.select("tbody");
-
-tableData.forEach(alien => {
-	var row =tbody.append("tr");
-	Object.entries(alien).forEach(([key,value]) => {
-		var cell = row.append("td");
-		cell.text(value);
-	});
-});
-
-
-var button = d3.select("button");
-button.on("click", function() {
-	console.log("click")
-	var inputElement = d3.select("#datetime");
-	var inputValue = inputElement.property("value");
-	if (inputValue != "") {
-		var filteredData = tableData.filter(alien => alien.datetime === inputValue);
-	}
-	tbody.html("")
-	filteredData.forEach(alien => {
-		var row = tbody.append("tr");
-		Object.entries(alien).forEach(([key, value]) => {
-			var cell = row.append("td");
-			cell.text(value);
-		});
-	});
+console.log(7)
+d3.json(url, {mode: 'no-cors'}).then(function(data){
+	console.log(data.Year)
 });
