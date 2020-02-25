767df14af75b49195236f74f898cf8edd4e3986f
David Nassif <David.Nassif@NexTierOFS.com>
2020-02-25 17:42:48 -0600

    trying heroku

--- webpage/static/js/app.js
+++ webpage/static/js/app.js
@@ -1,6 +1,10 @@
 var url='http://127.0.0.1:5000/api/v1.0/all_winners_data'
 
-console.log(7)
+// console.log(7)
 d3.json(url, {mode: 'no-cors'}).then(function(data){
-	console.log(data.Year)
+	console.log(data)
 });
+
+// d3.json(url, {mode:'no-cors'}, function(data) {
+//     console.log(data);
+// });
