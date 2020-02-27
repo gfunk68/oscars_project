954137410ac8b315cfecbd7efe15e5acf705c147
Kasie Wong <kasiewong@Kasies-MacBook-Pro.local>
2020-02-27 01:04:17 -0600

    updating age_years_chart related  code

--- Boot strap _web page/static/js/age_years_chart.js
+++ Boot strap _web page/static/js/age_years_chart.js
@@ -1,89 +1,70 @@
 // Creating our initial map object
 var url = "https://oscars-dataset.herokuapp.com/api/v1.0/all_winners_data"
 
-  d3.json(url,function(d){
-
-
-    winnerz=[]
-      
+d3.json(url, function(d){
+    
+    // Declare variables as arrays
+    var years =[];
+    var age = [];
+    var category =[];
+    var name =[];
+    var colors =[];
+    
+    // Create foreach loop to append values to respective arrays
     d.forEach(function(winner) {
-        if (winnerz.indexOf(winner['Winner Name']) !== -1)
-        {return;}
-        
-        winnerz.push(winner['Winner Name']);
+      years.push(winner['Year']);
+      age.push(winner['Age Awarded']);
+      category.push(winner['Category']);
+      name.push(winner['Winner Name']);
 
-        if (winner['Age Awarded'] ==='Unknown')
-        { return; }
-
-
-      var colors = "";
-      
-        if (winner['Category'] == 'Best Actor' || winner['Category'] == 'Best Supporting Actor'){
-          colors = "blue";
-        }
-        else if (winner['Category'] == 'Best Director') {
-            colors = 'green';
-        }
-        else {
-          colors = "pink";
-        }
-        
-        // console.log(winner)
-
-    // find unique years for plotting
-    // const onlyUnique = (value, index, self) => { 
-    //   return self.indexOf(value) === index;
-    // }
-    // const years = winner['Year'];
-    // const uniqueyears = years.filter(onlyUnique);
-    var years = winner['Year'];
-
-    // console.log(uniqueyears)
-
-    var age = winner['Age Awarded'];
-    // console.log(age)
-
-    var category = winner['Category'];
-    // console.log(category)
-
-    var name = winner['Winner Name'];
-    // console.log(name)
+      if (winner['Category'] == 'Best Actor' || winner['Category'] == 'Best Supporting Actor'){
+        colors.push('blue');
+      }
+      else if (winner['Category'] == 'Best Director') {
+        colors.push('green');
+      }
+      else {
+        colors.push('pink');
+      }
+    });
     
-        var trace = {
-        x: [years],
-        y: [age],
-        mode: "markers",
-        name: category,
-        text: name,
-        marker: {
-          color: colors,
-          size: 12,
-          line: {
-            width: 0.5
-          }
-        },
-        type: "scatter"
-      };
-      
-      var data = [trace];
-      var layout = {
-        title: "Ages Awarded Over Time",
-        xaxis: {
-          title: "Years",
-          showgrid: false,
-          zeroline: false
-        },
-        yaxis: {
-          title: "Age Awarded",
-          showline: false
-        }
-      };
-      var graphOptions = {layout: layout, filename: "line-style", fileopt: "overwrite"};
-      Plotly.newPlot("age_years_chart", data, layout, {scrollZoom: true});
-      });
-      
-    
-})
-
-
-
+// Set graph characteristics and chart with Plotly
+    var trace = {
+    x: years,
+    y: age,
+    mode: "markers",
+    name: category,
+    text: name,
+    marker: {
+      color: colors,
+      size: 11,
+      line: {
+        width: 0.2
+      }
+    },
+    type: "scatter"
+  };
+  
+    var data = [trace];
+    var layout = {
+      title: "Ages Awarded Oscar Over Time",
+      titlefont: {size: 30},
+      hovermode: "closest",
+      xaxis: {
+        title: "Years",
+        titlefont: {size: 20},
+        tickfont:{size: 16},
+        automargin: true,
+        showgrid: false,
+        zeroline: false
+      },
+      yaxis: {
+        title: "Age Awarded",
+        titlefont: {size: 20},
+        tickfont:{size: 16},
+        automargin: true,
+        showline: false
+      }
+    };
+    Plotly.newPlot("age_years_chart", data, layout);
+  });
